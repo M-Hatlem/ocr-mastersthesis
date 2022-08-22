77f44a7eeba01651ca44ae3ef6616c4713d4f8cb
M-Hatlem <markushatl1999@gmail.com>
2022-08-22 11:27:25 +0200

    Added options for rescaling image when testing

--- Image detection.py
+++ Image detection.py
@@ -1,6 +1,23 @@
 import cv2
 import pytesseract
 import numpy as np
+import re as re
+
+#rezise image for showing
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
 
 #Draw a box around each letter
 def box_letters(img):
@@ -10,7 +27,8 @@ def box_letters(img):
         b = b.split(' ')
         img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
 
-    cv2.imshow('img', img)
+    resize = ResizeWithAspectRatio(img, height=1080)
+    cv2.imshow('resize', resize)
     cv2.waitKey(0)
 
 
@@ -25,7 +43,8 @@ def box_words(img):
             (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imshow('img', img)
+    resize = ResizeWithAspectRatio(img, height=1080)
+    cv2.imshow('resize', resize)
     cv2.waitKey(0)
 
 
@@ -44,7 +63,8 @@ def box_pattern(img):
                 (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv2.imshow('img', img)
+    resize = ResizeWithAspectRatio(img, height=1080)
+    cv2.imshow('resize', resize)
     cv2.waitKey(0)
 
 #pre-proecssing methods
@@ -68,10 +88,35 @@ def opening(image):
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
 
+def gray_dialate(img):
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # TODO Binarize!
+    img = canny(img)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    # Apply blur to smooth out the edges
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    return img
+
+
 if __name__ == "__main__":
     #img = cv2.imread("Images/IMG_0891.jpg")
     img = cv2.imread("Images/IMG_0892.jpg")
-    #img = canny(img)
+
+    #TESTING
+    #Rescale to 300DPI
+    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+
+
+    img = gray_dialate(img)
+
+
     box_words(img)
     #box_letters(img)
     #box_pattern(img)
+    print(pytesseract.image_to_string(img))
