412df3db7787e9d744c58b51085215d206d846cb
Markus Hatlem <markushatl1999@gmail.com>
2022-09-02 15:27:29 +0200

    Started work on pattern detection function prototype

--- Image detection.py
+++ Image detection.py
@@ -2,13 +2,10 @@ import cv2
 import pytesseract
 import numpy as np
 import re as re
+import pandas as pd
 
-def find_pattern(img):
-    text = pytesseract.image_to_string(img)
-    found = re.findall(r"HBE \d{5}\n\d{3} [O|B|C]\d{2}", text) # pattern here--- should be r"HBE \d{5}\n\d{3} [O|B|C]\d{2}$
-    return(found)
 
-#rezise image for showing
+# rezise image for showing, only needed when viewing in python. Can remove when propper UI is added.
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
@@ -24,34 +21,27 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
 
     return cv2.resize(image, dim, interpolation=inter)
 
-#Draw a box around each letter
-def box_letters(img, pro_img):
-    h, w, c = img.shape
-    boxes = pytesseract.image_to_boxes(img)
-    for b in boxes.splitlines():
-        b = b.split(' ')
-        img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 0, 255), 2)
 
-    resize = ResizeWithAspectRatio(img, height=1080)
-    cv2.imshow('resize', resize)
-    cv2.waitKey(0)
+def find_pattern_matches(img):
+    text = pytesseract.image_to_string(img)
+    found = re.findall(r"HBE \d{5}\n\d{3} [O|B|C]\d{2}", text) # pattern here--- should be r"HBE \d{5}\n\d{3} [O|B|C]\d{2}$
+    return found
 
 
-#Draw a box around each word
-def box_words(img, pro_img):
+# Draw a box around each word TODO Make sentences
+def box(img, pro_img):
     d = pytesseract.image_to_data(pro_img, output_type=pytesseract.Output.DICT)
-    #print(d.keys())
-
-    pattern = find_pattern(pro_img)
-    print(pattern)
-
+    pattern = r"HBE \d{5}\n\d{3} [O|B|C]\d{2}"
     n_boxes = len(d['text'])
     for i in range(n_boxes):
         if int(float(d['conf'][i])) > 60:
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            pro_img = cv2.rectangle(pro_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+            if re.match(pattern, d['text'][i]):
+                print(d['text'][i])
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 12)
+            else:
+                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 12)
     resize = ResizeWithAspectRatio(img, height=1080)
     cv2.imshow('resize', resize)
     pro_resize = ResizeWithAspectRatio(pro_img, height=1080)
@@ -59,47 +49,17 @@ def box_words(img, pro_img):
     cv2.waitKey(0)
 
 
-#Draw a box around a pattern
-def box_pattern(img, pro_img):
-    d = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-    keys = list(d.keys())
-
-    pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
-    #Pattern for date..
-
-    n_boxes = len(d['text'])
-    for i in range(n_boxes):
-        if int(d['conf'][i]) > 60:
-            if re.match(pattern, d['text'][i]):
-                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    resize = ResizeWithAspectRatio(img, height=1080)
-    cv2.imshow('resize', resize)
-    cv2.waitKey(0)
-
-#pre-proecssing methods
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
-#canny edge detection
-def canny(image):
-    return cv2.Canny(image, 150, 200, True)
-
-
 def gray_dialate(img):
 
-    # TODO remove color
+    # TODO remove color?
     #black = np.array([0, 0, 0])
     #img = cv2.inRange(img, black, black)
 
     # TODO Convert to gray
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-    #img = canny(img)
+    # Canny binarize alternative
+    #img = cv2.Canny(image, 150, 200, True)
     # TODO Binarize better!
     _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     img = cv2.bitwise_not(img)
@@ -128,14 +88,12 @@ if __name__ == "__main__":
 
 
     #print(pytesseract.image_to_string(pro_img)) # Prints the image text
-    box_words(img, pro_img)
-    #box_letters(img, pro_img)
-    #box_pattern(img, pro_img)
+    box(img, pro_img)
 
-    #pattern = r"HBE \d{5}\n\d{3} [O|B|C]\d{2}$"
 
     # TODO
     # Get image read
     # Find patterns of text from casettes # Patteren is HBE (HELSE BERGEN) - ID (5 digit number) - (new line) - sample number (3 digits) - department and year - B/A/C - 2 digits)
+    #pattern = r"HBE \d{5}\n\d{3} [O|B|C]\d{2}$"
     # report missing patterns
     # show correct patterns
