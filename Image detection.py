ba7fdbef098de4ef68dd191362af354b29de51ce
Markus Hatlem <markushatl1999@gmail.com>
2022-09-08 19:37:05 +0200

    Increased accuracy to around 30% in test image 0894

--- Image detection.py
+++ Image detection.py
@@ -22,30 +22,36 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
 
 
 # finds and returns the casettes with a recongizable pattern from an image
-def find_pattern_matches(img):
-    text = pytesseract.image_to_string(img)
+def find_pattern_matches(img, custom_config):
+    text = pytesseract.image_to_string(img, config=custom_config)
     found = re.findall(r"HBE \d{5}\n\d{3} [O|B|C]\d{2}", text) # pattern here--- should be r"HBE \d{5}\n\d{3} [O|B|C]\d{2}$ #
     # Patteren is HBE (HELSE BERGEN) - ID (5 digit number) - (new line) - sample number (3 digits) - department and year - B/A/C - 2 digits)
+    #print(found) # TODO TEST LINE
     return found
 
 
 # Draw a box around each sentence and show the image TODO Make sentences
 def box(img, pro_img):
-    d = pytesseract.image_to_data(pro_img, output_type=pytesseract.Output.DICT)
-    pattern = r"HBE\d{5}\d{3}[O|B|C]\d{2}$"
+    custom_config = r' -c tessedit_char_whitelist="HBEhbeOoCc0123456789 "' # TODO FIX
+    d = pytesseract.image_to_data(pro_img, output_type=pytesseract.Output.DICT, config=custom_config)
+    pattern = r"\s?HBE\d{5}\d{3}[O|B|C]\d{2}$"
+
+    #print((d['text'])) # TODO TEST LINE
 
     n_boxes = len(d['text'])
     for i in range(n_boxes):
         try:
             # Looks at each word and the five next words to see if it matches the pattern
             if re.match(pattern, d['text'][i] + d['text'][i + 1] + d['text'][i + 2] + d['text'][i + 3] + d['text'][i + 4]):
-                # print(d['text'][i] + d['text'][i + 1] + d['text'][i + 2] + d['text'][i + 3] + d['text'][i + 4])
+                #print(d['text'][i] + d['text'][i + 1] + d['text'][i + 2] + d['text'][i + 3] + d['text'][i + 4]) # TODO TEST LINE
                 # if there is a pattern match, get the coordinates draw a green box/rectangle the words.
                 (x, y) = (d['left'][i], d['top'][i])
                 (w1, h1, w2, h2) = (d['width'][i], d['height'][i], d['width'][i+1], d['height'][i+3])
                 img = cv2.rectangle(img, (x, y), (x + w1 + w2, y + h1 + h2), (0, 255, 0), 12)
         except:
             pass
+
+    print(len(find_pattern_matches(pro_img, custom_config))) # TODO TEST LINE
 
     #Display the images in python, can be removed once UI is added
     resize = ResizeWithAspectRatio(img, height=1080)
@@ -81,6 +87,7 @@ def pre_process(img):
 # Tests for running
 if __name__ == "__main__":
     #img = cv2.imread("Images/IMG_0891.jpg")
+    #img = cv2.imread("Images/IMG_0892.jpg")
     img = cv2.imread("Images/IMG_0894.jpg")
 
     #TESTING
@@ -90,7 +97,13 @@ if __name__ == "__main__":
 
     pro_img = pre_process(img)
 
-    #print(pytesseract.image_to_string(pro_img)) # Prints the image text
+    #Testing for PSM
+    #custom_config = r' -c tessedit_char_whitelist="HBEhbeOoCc0123456789 "#
+    #print(pytesseract.image_to_string(pro_img, config=custom_config)) # Prints the image text
+    #print(len(find_pattern_matches(pro_img, custom_config)))
+
+
+
     box(img, pro_img)
 
 
